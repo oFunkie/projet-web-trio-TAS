536ae054cba4600167c6c2090025c41125505def
Aurélien Barbarant <a.barbarant02@gmail.com>
2025-04-18 15:36:29 +0200

    ajout de la galerie

--- pages/vente.php
+++ pages/vente.php
@@ -1,5 +1,16 @@
 <?php include '../main/top.php'; ?>
 
-
+<main class="galerie">
+    <h2>Nos Fleurs</h2>
+    <div class="grid-galerie">
+        <?php for ($i = 1; $i <= 20; $i++): ?>
+            <div class="fleur">
+                <img src="<?= base ?>images/fleur<?= $i % 6 + 1 ?>.jpg" alt="Fleur <?= $i ?>">
+                <p>Fleur n°<?= $i ?></p>
+                <button>Ajouter au panier</button>
+            </div>
+        <?php endfor; ?>
+    </div>
+</main>
 
 <?php include '../main/bot.php'; ?>
