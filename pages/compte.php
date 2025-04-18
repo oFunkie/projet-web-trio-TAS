78edcd22d160089d448a41e4a67a994893bfd31b
Stev-E2006 <anticstevan06@gmail.com>
2025-04-18 16:05:59 +0200

    compte2

--- pages/compte.php
+++ pages/compte.php
@@ -1,4 +1,18 @@
 <?php include '../main/top.php'; ?>
 
+<tbody>
+    <?php 
+    include '../main/access.php'; 
+    $req = "SELECT * FROM client"; 
+    $reponse = $access->query($req); 
+    foreach ($reponse as $r): 
+    ?>
+        <p><?= $r['nom'] ?>
+        <?= $r['prenom'] ?>
+        <?= $r['adresse'] ?>
+        <?= $r['ville'] ?>
+        <?= $r['cp'] ?></p>
+    <?php endforeach; ?>
+</tbody>
 
 <?php include '../main/bot.php'; ?>
