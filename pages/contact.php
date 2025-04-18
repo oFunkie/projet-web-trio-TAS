618317ee66c0ed9c283bd7fa72c05c36e5ed027b
WRLD11 <thibaud.boulmier@gmail.com>
2025-04-18 14:54:37 +0200

    pages contact

---
+++ pages/contact.php
@@ -0,0 +1,5 @@
+<?php include '../main/top.php'; ?>
+<title>Contact</title>
+<p>Si vous rencontrer un probleme veuillez nous contacter a l'une des adresse suivante</p>
+
+<?php include '../main/bot.php'; ?>
