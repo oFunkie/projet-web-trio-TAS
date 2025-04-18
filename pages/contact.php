a9491a58fd90d20e7d0ebf80a845fdc1d364a6ef
WRLD11 <thibaud.boulmier@gmail.com>
2025-04-18 17:13:11 +0200

    suppr contact

--- pages/contact.php
+++
@@ -1,7 +0,0 @@
-<?php include '../main/top.php'; ?>
-<title>Contact</title>
-<p>Si vous rencontrer un probleme veuillez nous contacter a l'une des adresse suivante</p>
-<div class="contact-container">
-    
-  </div>
-<?php include '../main/bot.php'; ?>
